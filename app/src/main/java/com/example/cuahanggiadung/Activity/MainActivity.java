6e40a5ac818cd307c167911354a89c423ce37ac2
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-05-24 15:33:59 +0700

    up

--- app/src/main/java/com/example/cuahanggiadung/Activity/MainActivity.java
+++ app/src/main/java/com/example/cuahanggiadung/Activity/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ViewFlipper;
 
+import com.example.cuahanggiadung.Adapter.CategoryAdapter;
+import com.example.cuahanggiadung.Model.CategoryModel;
 import com.example.cuahanggiadung.R;
 import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -21,8 +25,12 @@ public class MainActivity extends AppCompatActivity {
     NavigationView navigationView;
     ListView listViewManHinhChinh;
     DrawerLayout drawerLayout;
-//    ArrayList<CategoryModel> arrCategoryModels;
-//    CategoryAdapter categoryAdapter;
+    ArrayList<CategoryModel> arrCategoryModels;
+    CategoryAdapter categoryAdapter;
+
+    int id = 0;
+    String nameCategory = "";
+    String imgCategory = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AnhXa();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper()
+    {
+        ArrayList<String> mangQuangCao = new ArrayList<>();
+        mangQuangCao.add("https://cdn.tgdd.vn/2021/11/CookDish/cac-loai-do-gia-dung-hien-dai-cho-can-bep-nha-ban-them-tien-avt-1200x676.jpg");
+        mangQuangCao.add("https://giadungsato.com/Uploads/images/giadungsato(2).jpg");
+        mangQuangCao.add("https://ominsu.com.vn/wp-content/uploads/2017/09/Anh-NOI-CHAO-.jpg");
+        for (int i = 0; i< mangQuangCao.size(); i++)
+        {
+            ImageView imageView = new ImageView(getApplicationContext());
+            Picasso.with(getApplicationContext()).load(mangQuangCao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
     }
 
     private void AnhXa()
