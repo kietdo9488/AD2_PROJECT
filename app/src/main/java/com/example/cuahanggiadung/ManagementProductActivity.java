4ec5fedc1df6e399a6cb5d05771d7737f9fba1c9
nguyenHoangKinh <hoangkinh.tdc2223@gmail.com>
2023-05-29 16:21:54 +0700

    cap nhat thêm 5 mang hinh

---
+++ app/src/main/java/com/example/cuahanggiadung/ManagementProductActivity.java
@@ -0,0 +1,14 @@
+package com.example.cuahanggiadung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagementProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_management_product);
+    }
+}
